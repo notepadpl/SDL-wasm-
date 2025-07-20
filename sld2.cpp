54e4afbb5abeb930d626c58789c5946915a77779
notepadpl <przychodzien12@gmail.com>
2025-07-20 13:21:45 +0200

    Update sld2.cpp

--- sld2.cpp
+++ sld2.cpp
@@ -387,9 +387,7 @@ int main(int argc, char *argv[]) {
 
             }
 
-            SDL_GL_CreateContext(*state->windo
-
-ws);
+            SDL_GL_CreateContext(*state->windows);
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
